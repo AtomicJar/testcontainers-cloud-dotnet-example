4d39642fbd9b9040f99b80be1b02190b863aef4c
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2022-10-26 10:34:40 +0200

    fix: Remove non whitespace character in PrettyString (issue does not occur on console)

--- tests/TestcontainersCloud.DotNetSample/PrettyStrings.cs
+++ tests/TestcontainersCloud.DotNetSample/PrettyStrings.cs
@@ -2,7 +2,7 @@
 
 public static class PrettyStrings
 {
-    public const string Logo = "✔                                                                                \n" +
+    public const string Logo = "                                                                                  \n" +
                                "                                         /                                        \n" +
                                "                                       /////////                                  \n" +
                                "                                    ///////////////                               \n" +
