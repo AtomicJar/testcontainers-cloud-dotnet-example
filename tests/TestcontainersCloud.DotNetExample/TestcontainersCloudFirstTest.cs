569c9402d12436d0d7f5a96dbadcad473ac68d3f
Kirill Merkushev <1964214+lanwen@users.noreply.github.com>
2025-02-18 01:05:26 +0100

    import

--- tests/TestcontainersCloud.DotNetExample/TestcontainersCloudFirstTest.cs
+++ tests/TestcontainersCloud.DotNetExample/TestcontainersCloudFirstTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data.Common;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
